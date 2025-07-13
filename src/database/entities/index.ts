cd22a5832c367ad25dffdcab5dda14a2a425db66
Hernane <hernanegoncalvesmk@gmail.com>
2025-07-13 09:58:26 -0300

    teste

--- src/database/entities/index.ts
+++
@@ -1,4 +0,0 @@
-export { UserEntity, UserRole, UserStatus } from './user.entity';
-export { UserSessionEntity } from './user-session.entity';
-export { UserSubscriptionEntity, SubscriptionStatus, PlanType } from './user-subscription.entity';
-export { PaymentEntity, PaymentStatus, PaymentMethod } from './payment.entity';
